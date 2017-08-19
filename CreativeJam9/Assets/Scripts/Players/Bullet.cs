48d1a4131ab722a4e040c172e907340271d244a5
DominicBg <DominicBg@users.noreply.github.com>
2017-08-19 02:53:54 -0400

    +Start waterdrop

--- CreativeJam9/Assets/Scripts/Players/Bullet.cs
+++ CreativeJam9/Assets/Scripts/Players/Bullet.cs
@@ -5,19 +5,20 @@ using UnityEngine;
 public class Bullet : MonoBehaviour {
 
 	public int playerID;
-	public int damage;
+	//public int damage;
 
 	Transform tr;
 	float speed;
 
-	public void InitialiseBullet(int _playerID, Vector3 direction, float _speed, int _damage)
+	public void InitialiseBullet(int _playerID, Vector3 direction, float _speed)
 	{
 		tr = GetComponent<Transform>();
 
 		tr.LookAt(tr.position + direction);
 		speed = _speed;
 		playerID = _playerID;
-		damage = _damage;
+		//damage = _damage;
+		Destroy(gameObject,2);
 	}
 
 	void Update()
--- CreativeJam9/Assets/Scripts/Players/PlayerController.cs
+++ CreativeJam9/Assets/Scripts/Players/PlayerController.cs
@@ -25,10 +25,20 @@ public class PlayerController : MonoBehaviour {
 	public float bulletSpeed;
 	public float cooldownShoot = 0.5f;
 	public float cooldownDash = 0.5f;
+	public float dashForce = 15;
+	public float knockbackForce = 15;
+	public int dropPercentage = 30;
+	//public int damage = 10;
 
-	public float knockbackMagnitude = 2;
-	public int damage = 10;
+	[Header("Scaling")]
+	public Vector3 basicScale = Vector3.one;
+	public Vector3 waterWeightScaling = Vector3.one;
+
+
+	public float slideDirectionFactor;
 	public float slideFactor;
+
+	private Vector3 lastMoveInputVector;
 	private Vector3 slideVector;
 
 	//Buttons
@@ -36,19 +46,23 @@ public class PlayerController : MonoBehaviour {
 	private bool onShootCooldown; 
 	private bool releasedShootTrigger = true;
 
-	private Vector3 dashDir;
 	private bool onDashCooldown; 
 	private bool releasedDashTrigger = true;
 
+	private bool isDead;
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody>(); 
 		tr = GetComponent<Transform>();
 		player = ReInput.players.GetPlayer(playerID);
+		UpdateWaterLevel();
 	}
 	
 	void Update () 
 	{
+		//if(isDead)
+		//	return;
+
 		InputMouvement();
 		InputAim();
 		RotateBase();
@@ -63,21 +77,23 @@ public class PlayerController : MonoBehaviour {
 		if(moveVector.magnitude > 1)
 			moveVector.Normalize();
 
+		//save for dash
+		lastMoveInputVector = moveVector;
 
 		if(moveVector.magnitude == 0)
 		{		
+			//slow down sliding
 			slideVector *= slideFactor;
 		}
 		else
 		{
-			slideVector += moveVector * slideFactor;
+			//change direction slide
+			slideVector += moveVector * slideDirectionFactor;
 			if(slideVector.magnitude > 1)
 				slideVector.Normalize();
 		}
 
 		tr.position += (moveVector + slideVector) * moveSpeed * Time.deltaTime;
-
-	
 	}
 	void InputAim()
 	{
@@ -135,7 +151,7 @@ public class PlayerController : MonoBehaviour {
 	}
 	void Dash()
 	{
-
+		rb.AddForce((lastMoveInputVector) * dashForce,ForceMode.Impulse);
 	}
 	IEnumerator CooldownDash()
 	{
@@ -144,22 +160,12 @@ public class PlayerController : MonoBehaviour {
 		onDashCooldown = false;
 	}
 
-
-
 	void ShootBullet()
 	{
 		GameObject bullet = Instantiate(bulletPrefab, cannonShootPosition.position, Quaternion.identity);
-		bullet.GetComponent<Bullet>().InitialiseBullet(playerID,baseTr.forward,bulletSpeed,damage);
+		bullet.GetComponent<Bullet>().InitialiseBullet(playerID,baseTr.forward,bulletSpeed);
 	}
 	#endregion
-
-	void OnTriggerEnter(Collider col)
-	{
-		if(col.CompareTag("Bullet"))
-		{
-			HitByBullet(col.GetComponent<Bullet>());
-		}
-	}
 
 	#region Bullet
 	void HitByBullet(Bullet bullet)
@@ -167,7 +173,7 @@ public class PlayerController : MonoBehaviour {
 		if(bullet.playerID != playerID)
 		{
 			//knockback
-			rb.AddForce((bullet.transform.position - tr.position) * knockbackMagnitude);
+			rb.AddForce((bullet.transform.forward) * knockbackForce,ForceMode.Impulse);
 			Damaged(bullet);
 		
 			Stunned();
@@ -176,7 +182,15 @@ public class PlayerController : MonoBehaviour {
 	}
 	void Damaged(Bullet bullet)
 	{
-		waterLevel -= bullet.damage;
+		//get hit > loose 15% of total
+		//int damageHit = (int)((float)waterLevel * ((float)dropPercentage/100));
+		int damageHit = GameManager.GiveIntPercent(dropPercentage,waterLevel).MinimumOne();
+
+		waterLevel -= damageHit;
+
+		GameManager.instance.SpawnWater(damageHit,tr.position,-bullet.transform.forward);
+		UpdateWaterLevel();
+	
 		if(waterLevel <= 0)
 		{
 			DeathPlayer();
@@ -192,7 +206,37 @@ public class PlayerController : MonoBehaviour {
 	}
 	void DeathPlayer()
 	{
-
+		isDead = true;
 	}
 	#endregion
+
+	void GrabWater(Water water)
+	{
+		waterLevel += water.waterLevel;
+		UpdateWaterLevel();
+		water.Consumed();
+	}
+
+	void UpdateWaterLevel()
+	{
+		if(waterLevel > 100)
+			waterLevel = 100;
+
+		tr.localScale = basicScale + ((float)waterLevel * waterWeightScaling);
+
+		UIManager.instance.AjustWaterLevel(playerID,waterLevel);
+	}
+
+	void OnTriggerEnter(Collider col)
+	{
+		if(col.CompareTag("Bullet"))
+		{
+			HitByBullet(col.GetComponent<Bullet>());
+		}
+		else if(col.CompareTag("Water"))
+		{
+			GrabWater(col.GetComponent<Water>());
+		}
+	}
+
 }
